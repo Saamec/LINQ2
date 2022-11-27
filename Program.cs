632a6a7461fc08c56423459069b6a074f274302d
«Ilya <333zero777@mail.ru>
2022-11-27 22:00:21 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var classes = new[]
+            {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+            var allStudents = GetAllStudents(classes);
+            
+            Console.WriteLine(string.Join(" ", allStudents));
+        }
+
+        static string[] GetAllStudents(Classroom[] classes)
+        {
+            var students = (from student in classes.SelectMany(c => c.Students)
+                           select student).ToArray();
+            return students;
+        }
+
+        public class Classroom
+        {
+            public List<string> Students = new List<string>();
+        }
+    }
+}
